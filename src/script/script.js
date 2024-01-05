baa2e8187bc24d6d2e19750c88b98f7dbc91d3c7
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-05 23:58:44 +0530

    updated version fix bug task works when click it two times

--- src/script/script.js
+++ src/script/script.js
@@ -1,71 +1,29 @@
 function showContent1() {
-  const content = document.getElementById("accordian-content-1");
-  const iconAnime = document.getElementById("btn-icon-1");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    iconAnime.style.transform = "rotateZ(0deg)";
-    content.style.display = "none";
-  }
+  const content = document.getElementById("accordian-btn-1");
+  content.classList.toggle("show");
 }
 
 function showContent2() {
-  var content = document.getElementById("accordian-content-2");
-  var iconAnime = document.getElementById("btn-icon-2");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    content.style.display = "none";
-    iconAnime.style.transform = "rotateZ(0deg)";
-  }
+  var content = document.getElementById("accordian-btn-2");
+  content.classList.toggle("show");
 }
 
 function showContent3() {
-  var content = document.getElementById("accordian-content-3");
-  var iconAnime = document.getElementById("btn-icon-3");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    content.style.display = "none";
-    iconAnime.style.transform = "rotateZ(0deg)";
-  }
+  var content = document.getElementById("accordian-btn-3");
+  content.classList.toggle("show");
 }
 
 function showContent4() {
-  var content = document.getElementById("accordian-content-4");
-  var iconAnime = document.getElementById("btn-icon-4");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    content.style.display = "none";
-    iconAnime.style.transform = "rotateZ(0deg)";
-  }
+  var content = document.getElementById("accordian-btn-4");
+  content.classList.toggle("show");
 }
 
 function showContent5() {
-  var content = document.getElementById("accordian-content-5");
-  var iconAnime = document.getElementById("btn-icon-5");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    content.style.display = "none";
-    iconAnime.style.transform = "rotateZ(0deg)";
-  }
+  var content = document.getElementById("accordian-btn-5");
+  content.classList.toggle("show");
 }
 
 function showContent6() {
-  var content = document.getElementById("accordian-content-6");
-  var iconAnime = document.getElementById("btn-icon-6");
-  if (content.style.display == "none") {
-    content.style.display = "block";
-    iconAnime.style.transform = "rotateZ(45deg)";
-  } else {
-    content.style.display = "none";
-    iconAnime.style.transform = "rotateZ(0deg)";
-  }
+  var content = document.getElementById("accordian-btn-6");
+  content.classList.toggle("show");
 }
