6343c8fa705aa8f93f126827f312750dbdeee578
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-05 23:13:57 +0530

    functionality added

---
+++ src/script/script.js
@@ -0,0 +1,71 @@
+function showContent1() {
+  const content = document.getElementById("accordian-content-1");
+  const iconAnime = document.getElementById("btn-icon-1");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    iconAnime.style.transform = "rotateZ(0deg)";
+    content.style.display = "none";
+  }
+}
+
+function showContent2() {
+  var content = document.getElementById("accordian-content-2");
+  var iconAnime = document.getElementById("btn-icon-2");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    content.style.display = "none";
+    iconAnime.style.transform = "rotateZ(0deg)";
+  }
+}
+
+function showContent3() {
+  var content = document.getElementById("accordian-content-3");
+  var iconAnime = document.getElementById("btn-icon-3");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    content.style.display = "none";
+    iconAnime.style.transform = "rotateZ(0deg)";
+  }
+}
+
+function showContent4() {
+  var content = document.getElementById("accordian-content-4");
+  var iconAnime = document.getElementById("btn-icon-4");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    content.style.display = "none";
+    iconAnime.style.transform = "rotateZ(0deg)";
+  }
+}
+
+function showContent5() {
+  var content = document.getElementById("accordian-content-5");
+  var iconAnime = document.getElementById("btn-icon-5");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    content.style.display = "none";
+    iconAnime.style.transform = "rotateZ(0deg)";
+  }
+}
+
+function showContent6() {
+  var content = document.getElementById("accordian-content-6");
+  var iconAnime = document.getElementById("btn-icon-6");
+  if (content.style.display == "none") {
+    content.style.display = "block";
+    iconAnime.style.transform = "rotateZ(45deg)";
+  } else {
+    content.style.display = "none";
+    iconAnime.style.transform = "rotateZ(0deg)";
+  }
+}
